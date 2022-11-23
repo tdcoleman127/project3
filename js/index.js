0e50f469a7ddf834494fcc7c56f90114feb3bf4b
Trent Coleman <69553545+CheckPlz3432@users.noreply.github.com>
2022-11-22 21:48:09 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 42.029070294972136, lng: -87.73009732985915 },
-    zoom: 15,
+    center: { lat: 42.058890, lng: -87.749071 },
+    zoom: 12,
   });
 
   new google.maps.Marker({
