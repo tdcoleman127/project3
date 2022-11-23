0e79f83a8cf83e320c6a263687b0e8bf25730113
Trent Coleman <69553545+CheckPlz3432@users.noreply.github.com>
2022-11-22 20:53:41 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,10 +4,16 @@ function initMap() {
     zoom: 11,
   });
   
+  const myLatLng = { lat: 42.05890269684054, lng: -87.74947443175276 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 9,
+    center: myLatLng,
+  });
+
   new google.maps.Marker({
     position: myLatLng,
     map,
-    title: "Hello World!",
+    title: "I've never been to this mall, but I want to go!",
   });
 }
 
