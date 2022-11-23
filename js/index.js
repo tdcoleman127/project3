cdb9a728d70bc7e3b097a5e1f0529938d345fffb
Trent Coleman <69553545+CheckPlz3432@users.noreply.github.com>
2022-11-22 20:59:51 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,17 +3,21 @@ function initMap() {
     center: { lat: 42.029070294972136, lng: -87.73009732985915 },
     zoom: 11,
   });
-  
-  const myLatLng = { lat: 42.05890269684054, lng: -87.74947443175276 };
+}
+
+window.initMap = initMap;
+
+function initMap() {
+  const myLatLng = { lat: -25.363, lng: 131.044 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 9,
+    zoom: 4,
     center: myLatLng,
   });
 
   new google.maps.Marker({
     position: myLatLng,
     map,
-    title: "I've never been to this mall, but I want to go!",
+    title: "Hello World!",
   });
 }
 
