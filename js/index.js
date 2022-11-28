3b04213614df708b05e6ead12575e28c5b12e337
Trent Coleman <69553545+CheckPlz3432@users.noreply.github.com>
2022-11-28 14:06:36 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,40 @@
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 42.058890, lng: -87.749071 },
     zoom: 12,
   });
 
-  new google.maps.Marker({
+  var mallMarker = new google.maps.Marker({
     position: { lat: 42.058890, lng: -87.749071 },
     map,
     title: "Mall!",
   });
   
-  new google.maps.Marker({
+  var townMarker = new google.maps.Marker({
     position: { lat: 42.009895, lng: -87.713161 },
     map,
     title: "Town Center!",
   });
 
-  new google.maps.Marker({
+  var parkMarker = new google.maps.Marker({
     position: { lat: 42.049187, lng: -87.672943 },
     map,
     title: "Big Park!",
   });
+  
+  const infowindow = new google.maps.InfoWindow({
+    content: "This is the Old Orchard Mall!! XOXOLOLLMAOGG"
+  });
+  
+  infowindow.open(map, mallMarker);
+
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  
+  
 }
 
 window.initMap = initMap;
